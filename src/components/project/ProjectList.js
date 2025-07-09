bd90be850a3eee957fc9dde49fe82a787eee3d7e
therahul1996 <rs.official1996@gmail.com>
2025-07-09 08:26:34 +0530

    feat: changes

--- src/components/project/ProjectList.js
+++ src/components/project/ProjectList.js
@@ -5,6 +5,14 @@ import ProjectBox from "../content/ProjectBox";
 const ProjectList = () => {
   return (
     <>
+      {/* {
+  //   "id": 23,
+  //   "iconurl": "/images/projects/axoma.png",
+  //   "name": "Axoma",
+  //   "urlLink": "https://www.axoma.ai/",
+  //   "title": "Axoma AI – GenAI-Powered Document Intelligence Platform",
+  //   "description": "Built and enhanced the front-end for Axoma AI, a GenAI-powered document intelligence platform allowing users to upload PDFs and ask natural language questions for context-aware answers."
+  // }, */}
       <p className="length-result">
         We Found {projectData.length} results for Projects
       </p>
